7125ae46f136002594f8e3cba53b0d0c539c390a
Hrudayesh22 <122723561+Hrudayesh22@users.noreply.github.com>
2023-12-12 17:17:09 +0530

    Update Login.jsx

--- my-app/src/components/Login.jsx
+++ my-app/src/components/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
     e.preventDefault();
     try{
       setLoading(true)
-      const response=await axios.post("http://localhost:3000/api/user/login",formData,{
+      const response=await axios.post("https://busy-rose-moth-vest.cyclic.cloud/api/user/login",formData,{
         headers : {
           'Content-type' : 'application/json'
         }
